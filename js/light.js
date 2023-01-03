ea234a1d58f9d47c62846312896747d0be3de4b4
YDG-0814 <YDG_0814@qq.com>
2023-01-03 23:58:12 +0800

    Site updated: 2023-01-03 23:58:11

--- js/light.js
+++ js/light.js
@@ -47,3 +47,10 @@ function changeColor() {
 
 // 开启计时器
 window.onload = setInterval(changeColor, 1200);
+// 防抖全局计时器
+let TT = null;    //time用来控制事件的触发
+// 防抖函数:fn->逻辑 time->防抖时间
+function debounce(fn, time) {
+    if (TT !== null) clearTimeout(TT);
+    TT = setTimeout(fn, time);
+}
